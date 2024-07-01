a2727579e4338d6a244e6522f928e4921aaf5689
ArgoMel <122796999+ArgoMel@users.noreply.github.com>
2024-07-01 16:41:35 +0900

    플레이어 조작

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D theRB;
+    private Platformer2d controls;
+    private Vector2 moveDir;
+    private bool isOnGround=false;
 
+    public Rigidbody2D theRB;
+    public Transform groundPoint;
+    public Animator anim;
+    public LayerMask whatIsGround;
     public float moveSpeed;
     public float jumpForce;
 
+    private void Awake()
+    {
+        controls = new Platformer2d();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
     void Start()
     {
-        
+        controls.Player.Move.performed += ctx => Move(ctx.ReadValue<Vector2>());
+        controls.Player.Jump.performed += ctx => Jump();
     }
 
     void Update()
     {
-        //theRB.velocity = new Vector2(x, theRB.velocity.y);
+        if (controls.Player.Move.IsPressed())
+        {
+            theRB.velocity = new Vector2(moveDir.x * moveSpeed, theRB.velocity.y);
+        }
+        if (theRB.velocity.x<0)
+        {
+            transform.localScale = new Vector3(-1f,1f,1f);
+        }
+        else if (theRB.velocity.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        isOnGround = Physics2D.OverlapCircle(groundPoint.position, 0.2f, whatIsGround);
+        UpdateAnim();
+    }
+
+    private void Move(Vector2 dir)
+    {
+        moveDir = dir;
+    }
+
+    private void Jump()
+    {
+        if (isOnGround)
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
+    }
+
+    private void UpdateAnim()
+    {
+        anim.SetBool("isOnGround", isOnGround);
+        anim.SetFloat("speed", Mathf.Abs(theRB.velocity.x));
     }
 }
